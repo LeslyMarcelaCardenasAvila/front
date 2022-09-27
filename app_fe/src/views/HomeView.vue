959f312e7e6feeec9428619c63fbcba113cb639d
Lesly Marcela Cárdenas Ávila <leslymarcelacardenas@gmail.com>
2022-09-26 21:57:53 -0500

    primer commit

---
+++ app_fe/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.greetings{
+margin: 0;
+padding: 0%;
+height: 100%;
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+.greetings h1{
+font-size: 50px;
+color: #283747;
+}
+.greetings span{
+color: crimson;
+font-weight: bold;
+}
+</style>
